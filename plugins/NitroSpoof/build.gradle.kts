31f0c38b2feadbe85173b1f34fdbf5563a6c1246
DeafThing <217824070+DeafThing@users.noreply.github.com>
2025-07-09 15:06:31 -0400

    NitroSpoof emoji upsell experiment logic added

--- plugins/NitroSpoof/build.gradle.kts
+++ plugins/NitroSpoof/build.gradle.kts
@@ -1,12 +1,16 @@
 description = "Use all emotes in any server without Nitro."
-version = "1.0.10"
+version = "1.0.11"
 
 aliucord.changelog.set("""
+  # 1.0.11
+  * Added emoji autocomplete upsell experiment, now you can use :[emoji name]: and it will work (By DeafThing)
+
   # 1.0.10
   * Added the lossless quality argument to the emoji URL to combat Blur (by RGBCube)
   * Added the emoji name argument to the URL to help Vencord properly display the emoji (by Delphox)
   * Fix emojis breaking when the server boost runs out (by Delphox)
   * Fix invalid emoji size breaking the final emoji URL (by RGBCube)
+
   # 1.0.9
   * Removed the troll and made NitroSpoof work again
   * Thanks to Lenk for helping me with the code
